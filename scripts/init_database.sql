68618683b6d120190401f0d876ae45f4ab5f3758
Aguti Catherine <114284982+caguti856@users.noreply.github.com>
2025-06-13 09:30:54 +0300

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,17 @@
+-- Database: Datawarehouse
 
+-- DROP DATABASE IF EXISTS "Datawarehouse";
+
+CREATE DATABASE "Datawarehouse"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en-US'
+    LC_CTYPE = 'en-US'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+CREATE SCHEMA Bronze;
+CREATE SCHEMA Silver;
+CREATE SCHEMA Gold;
